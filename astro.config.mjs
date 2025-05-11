e5c33b5d782d2868dcb6e3cbfd53d529bc1f6cea
Jeff Khuu <heyjeffkhuu@gmail.com>
2025-05-10 21:38:44 -0600

    github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jeffkhuu.github.io',
+  base: 'stats-demo',
   integrations: [react()]
 });
